6423faba66d72d6b38e68e0c5b8615d85857a667
abdikhaliqadam00 <abdikhaliqadam00@gmail.com>
2026-09-13 15:19:48 +0700

    Add NBA information and team details

--- python progress.py
+++ python progress.py
@@ -1,1 +1,30 @@
+#NBA
+sport = "Basketball"
+league = "NBA"
+
+print("Sport", sport)
+print("League", league)
+
+teams = ["Warriors", "Lakers", "Heat", "Wolves", "Celtics"]
+print ("Teams", teams)
+
+team = "Warriors"
+city = "Golden State"
+
+print ("Team", team)
+print ("City", city)
+
+players = { 
+         "Steph Curry": "Warriors",
+         "Lebron James": "Lakers",
+         "Ant Man": "Wolves"
+}
+
+
+
+
+
+
+
+
 
